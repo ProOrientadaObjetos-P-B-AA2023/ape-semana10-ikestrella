65ec10f7f97c97f544e36afe3e28301b76164373
ikestrella <116619952+ikestrella@users.noreply.github.com>
2023-06-17 14:28:52 -0500

    Trabajo

--- ManejoHerencia/src/paquete1/Ejecutor.java
+++ ManejoHerencia/src/paquete1/Ejecutor.java
@@ -5,10 +5,69 @@
  */
 package paquete1;
 
+import paquete3.PrestamoAutomovil;
+import paquete4.PrestamoEducativo;
+import paquete5.Persona;
+import paquete6.InstitucionEducativa;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejecutor {
-    
+    public static void main(String[] args) {
+        boolean inte=true;
+        Scanner sc= new Scanner(System.in);
+        ArrayList<PrestamoAutomovil>automovils= new ArrayList<>();
+        ArrayList<PrestamoEducativo>educativos= new ArrayList<>();
+        while (inte){
+            try {
+                System.out.println("1: INGRESAR DATOS PARA UN PRESTAMO DE AUTOMOVIL");
+                System.out.println("2: INGRESAR DATOS PARA UN PRESTAMO EDUCATIVO");
+                System.out.println("3: SALIR");
+                int opc= sc.nextInt();
+                switch (opc){
+                    case 1:
+                        Persona persona;
+                        Persona garante;
+                        System.out.println("INGRESE: NOMBRE, APELLIDO, USUARIO");
+                        persona=new Persona(sc.next(),sc.next(),sc.next());
+                        System.out.println("INGRESE GARANTE: NOMBRE; APELLIDO, USUARIO");
+                        garante=new Persona(sc.next(),sc.next(),sc.next());
+                        System.out.println("INGRESE: TIEMPO PRESTAMO;CIUDAD PRESTAMO ;TIPO AUTO; MARCA AUTO; VALOR AUTO");
+                        automovils.add(new PrestamoAutomovil(persona,sc.nextInt(),sc.next(),sc.next(),sc.next(),garante,sc.nextDouble()));
+                        break;
+                    case 2:
+                        Persona persona2;
+                        InstitucionEducativa institucion;
+                        System.out.println("INGRESE: NOMBRE, APELLIDO, USUARIO");
+                        persona2=new Persona(sc.next(),sc.next(),sc.next());
+                        System.out.println("INGRESE: NOMBRE INSTITUCION; SIGLAS");
+                        institucion=new InstitucionEducativa(sc.next(),sc.next());
+                        System.out.println("INGRESE: TIEMPO PRESTAMO; CIUDAD PRESTAMO; NIVEL ESTUDIO; VALOR CARRERA");
+                        educativos.add(new PrestamoEducativo(persona2,sc.nextInt(),sc.next(),sc.next(),institucion,sc.nextDouble()));
+                        break;
+                    case 3:
+                        System.out.println("PRESTAMOS AUTOMOVILES");
+                        for (PrestamoAutomovil pr:automovils) {
+                            pr.calcularvalorMensualPagoPrestamoAutomovil();
+                            System.out.println(pr);
+                        }
+                        System.out.println("PRESTAMOS EDUCATIVOS");
+                        for (PrestamoEducativo ins:educativos) {
+                            ins.calcularValorMensualPagoPrestamoCarrera();
+                            System.out.println(ins);
+                        }
+                        inte=false;
+                        break;
+                }
+            }catch (Exception err){
+                System.out.println("OPCION INVALIDA");
+                sc.next();
+            }
+        }
+    }
 }
--- ManejoHerencia/src/paquete2/Prestamo.java
+++ ManejoHerencia/src/paquete2/Prestamo.java
@@ -5,10 +5,56 @@
  */
 package paquete2;
 
+import paquete5.Persona;
+
 /**
  *
  * @author reroes
  */
 public class Prestamo {
-    
+    private Persona beneficiario;
+    private int tiempoPrestamo;
+    private String ciudadPrestamo;
+
+    public Prestamo() {
+    }
+
+    public Prestamo(Persona beneficiario, int tiempoPrestamo, String ciudadPrestamo) {
+        this.beneficiario = beneficiario;
+        this.tiempoPrestamo = tiempoPrestamo;
+        this.ciudadPrestamo = ciudadPrestamo.toLowerCase();
+    }
+
+    public Persona getBeneficiario() {
+        return beneficiario;
+    }
+
+    public void setBeneficiario(Persona beneficiario) {
+        this.beneficiario = beneficiario;
+    }
+
+    public int getTiempoPrestamo() {
+        return tiempoPrestamo;
+    }
+
+    public void setTiempoPrestamo(int tiempoPrestamo) {
+        this.tiempoPrestamo = tiempoPrestamo;
+    }
+
+    public String getCiudadPrestamo() {
+        return ciudadPrestamo;
+    }
+
+    public void setCiudadPrestamo(String ciudadPrestamo) {
+        this.ciudadPrestamo = ciudadPrestamo.toLowerCase();
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "beneficiario=" + beneficiario +
+                ", tiempoPrestamo=" + tiempoPrestamo +
+                ", ciudadPrestamo='" + ciudadPrestamo + '\'' +
+                '}';
+    }
 }
